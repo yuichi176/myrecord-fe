8001bbefcfe6ed5a25509109122b7837eaa43e88
Yuichi Sugiyama <ozwald176@gmail.com>
2023-04-13 20:44:17 +0900

    Fix env var name

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,13 +1,13 @@
 interface Env {
-  BFF_PROTOCOL?: string;
-  BFF_BASE_DOMAIN?: string;
-  BE_PROTOCOL?: string;
-  BE_BASE_DOMAIN?: string;
+  BFF_PROTOCOL?: string
+  BFF_BASE_DOMAIN?: string
+  BE_PROTOCOL?: string
+  BE_BASE_DOMAIN?: string
 }
 
 export const env = {
   BFF_PROTOCOL: process.env.BFF_PROTOCOL || 'http',
-  BFF_BASE_DOMAIN: process.env.BFF_BASE_URL || 'localhost:3000',
+  BFF_BASE_DOMAIN: process.env.BFF_BASE_DOMAIN || 'localhost:3000',
   BE_PROTOCOL: process.env.BE_PROTOCOL || 'http',
-  BE_BASE_DOMAIN: process.env.BE_BASE_URL || 'localhost:8080',
+  BE_BASE_DOMAIN: process.env.BE_BASE_DOMAIN || 'localhost:8080',
 } as Env
